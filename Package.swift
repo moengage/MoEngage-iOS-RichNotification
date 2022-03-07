509c5bdfd8dfbbe4f8a6e5592b0d2b0998221797
Rakshitha <>
2022-03-07 17:19:57 +0530

    Added initial setup

---
+++ Package.swift
@@ -0,0 +1,20 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "MoEngageRichNotification",
+    products: [
+        .library(
+            name: "MoEngageRichNotification",
+            targets: ["MoEngageRichNotificationSPM","MoEngageRichNotification"]),
+    ],
+    dependencies: [
+        .package(name: "MoEngage-iOS-SDK",url: "https://github.com/moengage/MoEngage-iOS-SDK.git", from: "8.0.0"),
+    ],
+    targets: [
+        .target(name: "MoEngageRichNotificationSPM", dependencies: ["MoEngage-iOS-SDK"], path: "Sources/..",exclude: ["MORichNotification.podspec", "README.md","./Frameworks/MoEngageRichNotification.xcframework", "LICENSE","CHANGELOG.md"]),
+        .binaryTarget(name: "MoEngageRichNotification",path: "./Frameworks/MoEngageRichNotification.xcframework")
+    ]
+)
